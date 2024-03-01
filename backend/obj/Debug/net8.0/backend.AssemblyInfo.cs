5542d3e29a29c9121ffbe38994da86eeba62d8c2
Aleks Regulski <aleks.regulski@icloud.com>
2024-03-01 15:46:05 +0100

    added gitignore

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b96ba85611245a9c0f2491da2a9b2f46805a633")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1af55329ad898c1704757d0aa93e905502719832")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
